93f5209ece388b0eb575f3097bb128f44b23613f
Gita Diaz <30728116+gitadiaz@users.noreply.github.com>
2019-06-16 10:34:59 +0700

    gita add location permission

--- app/src/main/java/bodoamat/samkuriang/fragment/MapsFragment.java
+++ app/src/main/java/bodoamat/samkuriang/fragment/MapsFragment.java
@@ -1,8 +1,12 @@
 package bodoamat.samkuriang.fragment;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +24,7 @@ public class MapsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_maps, container, false);
     }
